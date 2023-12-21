6b4def783190d0b707fae2b92cef6fab35b2420c
Jakob Staudal <jakob.staudal@lyniate.com>
2023-12-21 20:32:19 +0100

    Remove unused event variable

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
 
 		getSession();
 
-		const { data: authListener } = supabase.auth.onAuthStateChange(async (event, session) => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(async (_, session) => {
 			setSession(session);
 		});
 
